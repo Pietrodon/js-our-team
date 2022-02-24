29b805efc5770700aa38dd37fd5c5047d7b4da98
Pietrodon <donvitopietro00@gmail.com>
2022-02-24 16:50:33 +0100

    creazione  ciclo for e console.log degli elementi

--- our-team-base/app.js
+++ our-team-base/app.js
@@ -4,29 +4,41 @@ const team = [
       role: 'Founder & CEO',
       image: 'wayne-barnett-founder-ceo.jpg',
     },
+    
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+       name: 'Angela Caroll',
+       role: 'Chief Editor',
+       image: 'angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+       name: 'Walter Gordon',
+       role: 'Office Manager',
+       image: 'walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+       name: 'Angela Lopez',
+       role: 'Social Media Manager',
+       image: 'angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+       name: 'Scott Estrada',
+       role: 'Developer',
+       image: 'scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
-    },
-  ]
+       name: "Barbara Ramos",
+       role: "Graphic Designer",
+       image: "barbara-ramos-graphic-designer.jpg",
+    }
+  ];
+
+
+// ciclo for per ciclare elemnti array
+for (let i=0;i<team.length;i++){
+   console.log(team[i])
+}
+  
+  
+
+
+  
