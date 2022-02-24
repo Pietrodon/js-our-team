52fe9f3b8a479753cd51bc419010c15e82bbaa8f
Pietrodon <donvitopietro00@gmail.com>
2022-02-24 17:03:15 +0100

    creazione carta profilo e consolelog di essa

--- our-team-base/app.js
+++ our-team-base/app.js
@@ -37,10 +37,26 @@ const team = [
 for (let i=0;i<team.length;i++){
    console.log(team[i])
    // creazioni variabili per contenere i dati ciclati
-   let nome = team[i].name;
-   let ruolo =team[i].role;
-   let foto = team[i].image; 
+   const nome = team[i].name;
+   const ruolo =team[i].role;
+   const foto = team[i].image; 
    console.log(nome,ruolo,foto)
+
+   // creazione card
+   const card = `
+   <div class="team-card">
+      <div class="card-image">
+         <img
+         src="${foto}"
+         alt="${nome}"
+         />
+      </div>
+      <div class="card-text">
+         <h3>${nome}</h3>
+         <p>${ruolo}</p>
+      </div>
+   </div>`
+   console.log(card)
 }
   
   
